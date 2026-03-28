c1dd7ce93d3396318b542048bfeabdb973a89722
Nanomixer98 <nasabmx@hotmail.com>
2026-03-28 13:14:14 -0600

    feat: integrate a calculator frontend - Installed shadcn and tailwind css for components and styles - Incorporated calculator layout

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Calculator from "./Calculator";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>Hello world</StrictMode>,
+  <StrictMode>
+    <Calculator />
+  </StrictMode>,
 );
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
   ],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
